eec8201ed2bcf4aa3a4ff5440797839eb1217188
Mberdey <125443462+Mberdey@users.noreply.github.com>
2023-05-24 10:31:37 +0000

    Update my_shell.h

--- my_shell.h
+++ my_shell.h
@@ -7,10 +7,21 @@
 #include <unistd.h>
 #include <string.h>
 
+/* Function Prototypes */
 void print_prompt();
 char *read_input();
 void execute_command(char *command);
 void handle_errors();
 int main(__attribute__((unused))int argc, char **argv);
+void handle_cd(char **args);
+void execute_builtin_cmvd(char **args);
+void handle_exit(char **args);
+void handle_sigint(__attribute__((unused))int signum);
+void generate_prompt(void);
+int main(__attribute__((unused))int argc, char **argv);
+int is_builtin_cmvd(const char *command);
+int check_cmd_path(const char *cmvd, char *cmd_path, char *path_var);
+void store_path_var(char **path_var);
+void exect_cmd(char *cmd, char **argv, char *path_var);
 
 #endif
