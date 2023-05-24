a3cdb4f3acd7e93bf20d7f5931195c1d39511451
Mberdey <m.berdey@gmail.com>
2023-05-24 03:42:12 -0700

    Update

--- prompt.c
+++ prompt.c
@@ -2,7 +2,7 @@
 
 /**
  * generate_prompt - Generate the shell prompt
- * by lagsounemyy
+ * by Mourad Berdey
  */
 
 
@@ -11,6 +11,6 @@ void generate_prompt(void)
 	/*Check if reading from a terminal*/
 	if (isatty(STDIN_FILENO))
 	{
-		my_putstring("shellimi$ ");
+		my_putstring("shellMate$ ");
 	}
 }
